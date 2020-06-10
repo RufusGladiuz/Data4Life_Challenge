150eb8e94968e1cee897cb9b8982f74fcadf0e9a
RouvenReuter91 <rouven.reuter@viscopic.de>
2020-06-10 20:20:09 +0200

    Second Version

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import email
 import csv
 import os
-import logging
 import threading
 import time
 
+
+
 def sendMail(email):
     time.sleep(.5)
-    print("Send mail to {0}".format(email))
+    #print("Send mail to {0}".format(email))
 
+counter = 0
 threadList = []
 
 dataPath = os.path.join(r"E:\Projects\Data4life\assets", "mails.csv")
@@ -17,21 +18,26 @@ with open(file = dataPath) as csvfile:
     mails = csv.reader(csvfile, delimiter = ",")
     for row in mails:
         try:
+            counter += 1
             sendMailThread = threading.Thread(target = sendMail, args = (row[0],))
             sendMailThread.start()
             threadList.append(sendMailThread)
+
         except RuntimeError:
             initalLenght = len(threadList)
+            currentLenght = len(threadList)
 
-            while initalLenght >= len(threadList):
-
+            while initalLenght <= currentLenght:
                 for t in threadList:
-
-                    if not t.is_alive:
+                    if not t.is_alive():
                         threadList.remove(t)
-                       
+                        currentLenght = len(threadList)
+            time.sleep(.1)
+            print(counter)
             sendMailThread = threading.Thread(target = sendMail, args = (row[0],))
             sendMailThread.start()
             threadList.append(sendMailThread)
-            
+
+time.sleep(1)
+print("Send Mails: "+ str(counter))
 
