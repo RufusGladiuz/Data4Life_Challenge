f2bf1a94f5237651eba6ee465eb57eeaded73f62
RouvenReuter91 <rouven.reuter@viscopic.de>
2020-06-10 20:32:00 +0200

    ThreadPoolExecutor

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import csv
 import os
 import threading
 import time
-
+from concurrent.futures import ThreadPoolExecutor
 
 
 def sendMail(email):
     time.sleep(.5)
-    #print("Send mail to {0}".format(email))
+    print("Send mail to {0}".format(email))
 
 counter = 0
 threadList = []
@@ -15,28 +15,11 @@ threadList = []
 dataPath = os.path.join(r"E:\Projects\Data4life\assets", "mails.csv")
 
 with open(file = dataPath) as csvfile:
+    executor = ThreadPoolExecutor(max_workers=150)
     mails = csv.reader(csvfile, delimiter = ",")
+
     for row in mails:
-        try:
-            counter += 1
-            sendMailThread = threading.Thread(target = sendMail, args = (row[0],))
-            sendMailThread.start()
-            threadList.append(sendMailThread)
-
-        except RuntimeError:
-            initalLenght = len(threadList)
-            currentLenght = len(threadList)
-
-            while initalLenght <= currentLenght:
-                for t in threadList:
-                    if not t.is_alive():
-                        threadList.remove(t)
-                        currentLenght = len(threadList)
-            time.sleep(.1)
-            print(counter)
-            sendMailThread = threading.Thread(target = sendMail, args = (row[0],))
-            sendMailThread.start()
-            threadList.append(sendMailThread)
+        executor.submit(sendMail, row[0])
 
 time.sleep(1)
 print("Send Mails: "+ str(counter))
